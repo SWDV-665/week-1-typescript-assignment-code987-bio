08a4d0712544df5bded29c3ecd8a49395e9a4158
code987-bio <61991534+code987-bio@users.noreply.github.com>
2020-10-27 13:47:34 -0500

    Add files via upload

---
+++ grocery.ts
@@ -0,0 +1,6 @@
+class Grocery {
+    constructor(name: any, quantity: any, price: any, quality: any) {
+    this.name = name;
+    this.quantity = quantity;
+    } // When I add this.price and this.quality it changes what is after the equal sign to something else
+}
